19cf45436d49db3f6963107550b7d30d99f78555
Henrique 8086 <69872403+hsilva86@users.noreply.github.com>
2025-03-28 11:50:42 -0300

    Create 6_1busca.c
    algoritmo do exercicio 6.1

---
+++ 6_1busca.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int busca(int vet[], int n, int x) {
+    int i;
+    for (i=0;i<n;i++){
+        if(vet[i] == x){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int i,x = 0,resp,bigvet[100];
+    for(i=0;i<sizeof(bigvet);i++){
+        scanf("%d",&x);
+        if(x < 0)
+            break;
+        else
+            bigvet[i] = x;
+    }
+    int y = 0;
+    while (y>=0) {
+        scanf("%d",&y);
+        if(y <= 0) break;
+        resp = busca(bigvet,i,y);
+        printf(" %d ",resp);
+    }
+}
