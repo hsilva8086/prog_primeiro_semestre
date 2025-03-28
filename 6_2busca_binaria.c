eb3d6cb66940a6c14d8470c0a6006a580bb47c3b
Henrique 8086 <69872403+hsilva8086@users.noreply.github.com>
2025-03-28 13:04:25 -0300

    Update 6_2busca_binaria.c

--- 6_2busca_binaria.c
+++ 6_2busca_binaria.c
@@ -2,33 +2,40 @@
 int busca_binaria(int vet[], int n, int x) {
     int i,ini,fim,meio;
     ini = 0;
-    fim = sizeof(vet);
+    fim = n;
     
-    for (i=0; i<fim; i++){
+    for (i=0; i<fim; i++) {
         meio = (ini + fim) / 2;
-        print("%d %d\n", ini, fim);
-        if(vet[i] == x){
+        printf("%d %d\n", ini, fim);
+        if(vet[i] == x) {
             return i;
         }
+        if (x < meio){
+            fim = meio;
+        } else {
+            inicio = meio;
+        }
     }
-    
     return -1;
 }
 
 int main() {
+    //criar e alimentar o vetor bigvet com valores em ordem crescente(a order esta cargo do usuario).
     int i,x = 0,resp,bigvet[100];
-    for(i=0;i<sizeof(bigvet);i++){
+    for(i = 0; i < sizeof(bigvet); i++){
         scanf("%d",&x);
         if(x < 0)
             break;
         else
             bigvet[i] = x;
     }
+    //rotina de busca dos valores em busca_binaria ate que um valor negativo seja informado.
     int y = 0;
     while (y>=0) {
         scanf("%d",&y);
         if(y <= 0) break;
-        resp = busca(bigvet,i,y);
+        resp = busca_binaria(bigvet,i,y);
         printf(" %d ",resp);
     }
+    return 0;
 }
