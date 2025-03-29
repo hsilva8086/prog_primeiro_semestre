fc62e5125d87aa70c8fd3900d0a25947726b05df
Henrique 8086 <69872403+hsilva8086@users.noreply.github.com>
2025-03-28 21:29:40 -0300

    Update 6_3ordenar_pelo_menor.c
    só mais um pouco.

--- 6_3ordenar_pelo_menor.c
+++ 6_3ordenar_pelo_menor.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int determineIndiceDoMenor(int vet[], int ini, int fim, int menor){
-    int i,aux;
-    for(i = ini; i < fim; i++){
-        if(vet[i] < menor) {
+int determineIndiceDoMenor(int vet[], int ini, int fim){
+    int i,menor = 0;
+    for(i = ini; i <= fim; i++){
+        if(vet[i] < vet[menor]) {
             menor = i;
         } else {
             continue;
@@ -13,18 +14,32 @@ int determineIndiceDoMenor(int vet[], int ini, int fim, int menor){
 }
     
 int main() {
-    int vet[5] = { 1, 5, 2, 0, 1 };
+    int vet[5] = { 1, 5, 2, 0, -1 };
+    int j,k;
+    int aux;
     int menor = vet[0];
+    int xmenor = menor;
     int ini = 0;
     int fim = sizeof(vet)/4-1;
-    int j,k;
-    for(j = 0; j<fim ; j++) {
-        menor = determineIndiceDoMenor(vet, ini,fim, menor);
-        printf("%d %d\n",menor, vet[menor]);        
+    
+    //bloco de repeticao para ordenar o vetor
+    for(k=ini;k<=fim;k++) {
+        xmenor = determineIndiceDoMenor(vet, k, fim);
+        printf("%d %d\n",xmenor, vet[xmenor]); 
+    
+        if(xmenor != menor){
+            aux = vet[k];
+        
+            vet[k] = vet[xmenor];
+            vet[xmenor] = aux;
+        }
     }
-    for(k = 0;k<=fim;k++){
-        printf("%d ",vet[k]);        
+    //fim do bloco de repeticao
+    
+    //imprimir o vetor
+    for(j = 0; j <= fim; j++){
+        printf("%d, ",vet[j]);
     }
-
+        
     return 0;
 }
