6ffabc8118dc9bd456744924ebc36900f7675c79
Henrique 8086 <69872403+hsilva8086@users.noreply.github.com>
2025-03-29 07:53:49 -0300

    Update 6_3ordenar_pelo_menor.c
    Ainda falha em alguns testes.

--- 6_3ordenar_pelo_menor.c
+++ 6_3ordenar_pelo_menor.c
@@ -2,42 +2,50 @@
 #include <stdlib.h>
 
 int determineIndiceDoMenor(int vet[], int ini, int fim) {
-    int i,menor, aux;
-    for(i = ini; i <= fim; i++) {
-        if(vet[i] < vet[ini]) {
-            menor = i;
-        } else {
-            menor = ini;
-        }
-    }
+	int i,menor, aux;
+	for(i = ini; i <= fim; i++) {
+		if(vet[i] < vet[ini]) {
+			menor = i;
+		} else {
+			menor = ini;
+		}
+	}
 
-    printf("%d %d\n",menor, vet[menor]);
-   // printf("A=> ini:%d menor:%d |",vet[ini], vet[menor]);
-    if(vet[menor] < vet[ini]){
-        aux = vet[ini];
-        vet[ini] = vet[menor];
-        vet[menor] = aux;
-    }
-    //printf("B=> ini:%d menor:%d |",vet[ini], vet[menor]);
-    return menor;
+	printf("%d %d\n",menor, vet[menor]);
+
+	if(vet[menor] < vet[ini]) {
+		aux = vet[ini];
+		vet[ini] = vet[menor];
+		vet[menor] = aux;
+	}
+
+	return menor;
 }
-    
+
 int main() {
-    int vet[5] = { 1, 5, 2, 2, 1 };
-    int j;
-    int ini = 0;
-    int fim = sizeof(vet)/4-1;
+	int *vet;
+	int i,j,n;
+	int ini = 0;
+	int fim;
+
+	scanf("%d",&n);
+    vet = (int *) malloc(n * sizeof(int)); //determina dinamicamente o tamanho do vetor usando malloc.
+    for (i = 0; i < n; i++) {
+        scanf("%d",&vet[i]);
+    }
     
-    //bloco de repeticao para ordenar o vetor
-    ini = 0;
-    while(ini < fim)
-        determineIndiceDoMenor(vet, ini++, fim);
-    //fim do bloco de repeticao
-    
-    //imprimir o vetor
-    printf("\n");
-    for(j = 0; j <= fim; j++)
-        printf("%d ",vet[j]);
-        
-    return 0;
+	//bloco de repeticao para ordenar o vetor.
+	ini = 0;
+	fim = n-1;
+	while(ini < fim) {
+		determineIndiceDoMenor(vet, ini++, fim);
+	}
+	//fim do bloco de repeticao.
+
+	//imprimir o vetor.
+	printf("\n");
+	for(j = 0; j <= fim; j++)
+		printf("%d ",vet[j]);
+
+	return 0;
 }
