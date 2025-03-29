9be7c82a2fb62974351a0143dccecb860a3d2ad2
Henrique 8086 <69872403+hsilva8086@users.noreply.github.com>
2025-03-29 15:49:12 -0300

    Update 5.2ordem_inteiros.c
    corrigido para passar no teste c>a, mas b = a.

--- 5.2ordem_inteiros.c
+++ 5.2ordem_inteiros.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int main(){
+int main() {
     int a,b,c;
     
-    scanf("%d", &a);
-    scanf("%d", &b);
-    scanf("%d", &c);
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
     
     if(a>b && b>c){
         printf("%d %d %d",a,b,c);
-    } else if (a>c && c>b) {
+    } else if(a>c && c>b) {
         printf("%d %d %d",a,c,b);
     } else if(b>a && a>c) {
         printf("%d %d %d",b,a,c);
@@ -18,8 +18,9 @@ int main(){
         printf("%d %d %d",c,a,b);
     } else if(c>b && b>a) {
         printf("%d %d %d",c,b,a);
+    }else if(c>a) {
+        printf("%d %d %d",c,b,a);
     } else {
         printf("%d %d %d",a,b,c);
     }
-    return 0;
 }
